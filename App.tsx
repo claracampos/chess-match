4067417f4e18ec44be6da09796ec2574b0d58ce6
claracampos <camposclr@gmail.com>
2021-02-07 19:53:37 -0400

    add start screen and chess screen to app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { ChessScreen } from "./components/ChessScreen";
+import { StartScreen } from "./components/StartScreen";
+import { Color } from "./components/types";
 
 export default function App() {
+  const [player, setPlayer] = useState<Color | undefined>(undefined);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {player ? (
+        <ChessScreen player={player} />
+      ) : (
+        <StartScreen setPlayer={setPlayer} />
+      )}
     </View>
   );
 }
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
