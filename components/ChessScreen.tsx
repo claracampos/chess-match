73442559252a3f8ef1e008af572ce79e3c3f1194
claracampos <camposclr@gmail.com>
2021-02-10 19:23:03 -0400

    add player/computer game functionality

--- components/ChessScreen.tsx
+++ components/ChessScreen.tsx
@@ -1,16 +1,50 @@
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import { Chessboard } from "./Chessboard";
 import { Color } from "./types";
+import { capablanca, bogoljubov } from "../lib/matchMoves";
 
 interface StartScreenProps {
   player: Color;
 }
 
 export const ChessScreen = ({ player }: StartScreenProps) => {
+  let computer: Color = "black";
+  let playersMoves = capablanca;
+  let computersMoves = bogoljubov;
+  if (player === "black") {
+    computer = "white";
+    playersMoves = bogoljubov;
+    computersMoves = capablanca;
+  }
+
+  const [playersTurn, setPlayersTurn] = useState(player === "white");
+  const turnRef = useRef(playersTurn);
+  useEffect(() => {
+    if (turnRef.current !== playersTurn) {
+      if (playersTurn) {
+        computersMoves.shift();
+      } else {
+        playersMoves.shift();
+      }
+      turnRef.current = playersTurn;
+    }
+  });
+
+  const currentMove = playersTurn ? playersMoves[0] : computersMoves[0];
   return (
     <View style={styles.container}>
-      <Chessboard player={player} />
+      {currentMove ? (
+        <Chessboard
+          player={playersTurn ? player : computer}
+          playersView={player}
+          playersTurn={playersTurn}
+          setPlayersTurn={setPlayersTurn}
+          currentMove={currentMove}
+        />
+      ) : (
+        <Text>End of match</Text>
+      )}
     </View>
   );
 };
